77da5e483fa91f48450eb19f96afccd6a153e1f6
Hackman-Megatron <patrickstephanentanta@gmail.com>
2024-05-02 12:58:57 +0100

    it is committed

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,8 +1,32 @@
+import 'package:chatbot/views/mails_loggers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+      Future.delayed(
+          const Duration(seconds: 20),
+        () {
+            setState(() {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context)=> const SplashScreen()
+                  )
+              );
+            });
+        }
+      );
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -13,24 +37,42 @@ class SplashScreen extends StatelessWidget {
         decoration: const BoxDecoration(
           gradient: LinearGradient(
               colors: [
+                Color.fromARGB(255, 0, 255, 89),
                 Color.fromARGB(255, 0, 0, 0),
-                Color.fromARGB(255, 0, 255, 89)
               ],
-              begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+              begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
           )
         ),
-        child: const Column(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 50,),
-
-            SizedBox(height: 50,),
-            SpinKitCubeGrid(
-              color: Colors.white,
+            Text("ChatBot",
+              style: GoogleFonts.lato(
+                textStyle: const TextStyle(
+                  fontSize: 70,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.white
+                ),
+              ),
             ),
-            SizedBox(height: 50,),
-
-            SizedBox(height: 50,),
+            const SizedBox(height: 100,),
+            const SpinKitCubeGrid(
+              color: Colors.white,
+              size: 75,
+            ),
+            const SizedBox(height: 100,),
+            Text("By The Flutter Group 12",
+              style: GoogleFonts.lato(
+                textStyle: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white
+                ),
+              ),
+            ),
           ],
         ),
       ),
