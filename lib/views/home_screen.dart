00022b857e2cfc119f65c960cdf7ef839d76dff2
Hackman-Megatron <patrickstephanentanta@gmail.com>
2024-05-02 13:05:42 +0100

    megatron committed it

---
+++ lib/views/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeLand extends StatefulWidget {
+  const HomeLand({super.key});
+
+  @override
+  State<HomeLand> createState() => _HomeLandState();
+}
+
+class _HomeLandState extends State<HomeLand> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
